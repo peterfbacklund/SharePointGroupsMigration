2915ceddeb2dde861dfca2baee7812e1d41cbb03
Peter Backlund <peter@backlund.co>
2020-04-01 10:33:34 -0400

    changed write

--- Export-Group.ps1
+++ Export-Group.ps1
@@ -1,30 +1,28 @@
 #Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
 #Input variables
-$SourceWebURL = "http://cgfsaudit.cgfsdc.state.sbu/sites/finaudit"
-#$TargetWebURL = "http://auditst.cgfsdc.state.sbu/sites/finaudit"
-#In my scenario I will add a SharePoint group as the groups owner but you choose to use a user instead, make sure the user or group is available
-#in the destination site collection
-#$ownername = "your owner group name"
-#below define your string on which you filter for the groups to copy. You can choose to copy all groups or create your own filtering. 
-#I filtered for groups which match a certain string. Change this based on your needs.
-#$yourstring = "*something*"
-#Get the Webs
-$SourceWeb = Get-SPsite $SourceWebURL
-#$TargetWeb= Get-SPWeb $TargetWebURL
-#$owner = $targetWeb.groups[$ownername]
-#Get the Source groups
-$SourceGroup = $SourceWeb.rootweb.sitegroups #| where {$_.name -like ($yourstring) }
-$SourceGroup | Export-Csv -Path .\groups.csv
+$WebURL = "http://cgfsaudit.cgfsdc.state.sbu/sites/finaudit"
 
-foreach ($group in $SourceGroup) {
-    $group.users | Export-Csv -Path ".\groupUsers\" + $group.Name + ".csv"
-}
-#foreach ($group in $SourceGroup)
-#{
-# $TargetWeb.SiteGroups.Add($group.Name, $owner, $null, $group.description)
-# $destinationGroup = $TargetWeb.SiteGroups["$Group"]
-# $destinationGroup.owner = $owner
-# $destinationGroup.Update()
-#foreach ($user in $Group.users){$TargetWeb.SiteGroups["$Group"].AddUser($user)}
-# $destinationGroup.Update()
-#}
+$Web = Get-SPWeb $WebURL
+  
+ #Get all Groups and Iterate through   
+ foreach ($group in $Web.groups)
+ {
+    #Get Permission Levels Applied to the Group  
+    $RoleAssignment = $Web.RoleAssignments.GetAssignmentByPrincipal($group)
+ 
+    $RoleDefinitionNames=""
+    foreach ($RoleDefinition in $RoleAssignment.RoleDefinitionBindings)
+    { 
+        $RoleDefinitionNames+=$RoleDefinition.Name+";"
+    }
+     
+    write-host "Group Name: $($Group.name) : Permissions: $($RoleDefinitionNames) ----"
+    #Iterate through Each User in the group
+        foreach ($user in $group.users)
+        {
+            write-host $user.name  "`t" $user.LoginName  "`t"  $user.Email  | FT
+        }
+ } 
+
+
+#Read more: https://www.sharepointdiary.com/2013/07/export-sharepoint-users-and-groups-to-excel-using-powershell.html#ixzz6IIijYh48
