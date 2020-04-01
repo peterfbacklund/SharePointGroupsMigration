fd28371523a78b3ac8ba51a668a10ec4c48c52ee
Peter Backlund <peter@backlund.co>
2020-04-01 13:03:32 -0400

    Broke out import users from import groups

--- Import-Group.ps1
+++ Import-Group.ps1
@@ -15,18 +15,3 @@ foreach ($group in $sourcegroups) {
     $TargetWeb.SiteGroups.add($group.Name, $owner, $TargetWeb.site.owner, $group.description)
   }
 }
-
-
-$usergroupmembers = Import-Csv ./usergroupmembers.csv
-
-foreach ($user in $usergroupmembers) {
-    Write-Output $user.LoginName
-    $veruser = $TargetWeb.EnsureUser($user.LoginName)
-    if ($veruser -ne $null) {
-
-        $group = $TargetWeb.SiteGroups[$user.Group]
-        #Write-Output $group
-        $group.AddUser($veruser)
-    }
-   
-}
