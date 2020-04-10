02eefb32fde3513681ed66e7278d7a99187deea8
Peter Backlund <peter@backlund.co>
2020-04-10 13:17:20 -0400

    Added generic list export

--- Export-Group.ps1
+++ Export-Group.ps1
@@ -1,10 +1,18 @@
-$WebURL = "http://"
+Remove-Item -path .\groups.csv
+Remove-Item -path .\groupmembers.csv
+Remove-Item -path .\grouproles.csv
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+#Input variables
+$WebURL = "http://cgfs13index1dev/"
 
 $Web = Get-SPWeb $WebURL
 
 $web.Groups | Export-Csv -Path ./groups.csv
 
-"Group,LoginName,StrippedLoginName" | Out-File "./usergroupmembers.csv" -append
+"Group,LoginName,StrippedLoginName" | Out-File "./groupmembers.csv" -append
+
+"Group,Name" | Out-File "./grouproles.csv" -append
 
  #Get all Groups and Iterate through   
  foreach ($group in $Web.groups)
@@ -12,7 +20,18 @@ $web.Groups | Export-Csv -Path ./groups.csv
     # Dump all the groups/users into a csv file
         foreach ($user in $group.users)
         {
-            Write-Output $user.LoginName
-            $group.name + "," + $user.LoginName + "," + $user.LoginName.split("|")[1] | Out-File "./usergroupmembers.csv" -append
+            Write-Output $user.LoginName 
+            $group.name + "," + $user.LoginName + "," + $user.LoginName.split("|")[1] | Out-File "./groupmembers.csv" -append
         }
- } 
+
+        # Dump all the roles/permissions into a csv file
+        foreach ($role in $group.Roles)
+        {
+            #Write-Output $role.xml | FT
+            $group.name + "," + $role.Name | Out-File "./grouproles.csv" -append
+        }
+} 
+
+#if ([Microsoft.SharePoint.Publishing.PublishingWeb]::IsPublishingWeb($web)) {
+Get-spweb $web.Url | Get-PublishingPages | Export-Csv -Path ./pagemembers.csv
+#}
--- Import-Group.ps1
+++ Import-Group.ps1
@@ -1,7 +1,10 @@
-$TargetURL = ""
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue 
+
+$TargetURL = "http://cgfs13index1dev/sites/GroupMigrateTest"
 $TargetWeb = Get-SPWeb $TargetURL
 
-$ownername = "Financial Audit Site Owners"
+$ownername = "GroupMigrateTest Owners"
 $owner = $targetWeb.groups[$ownername]
 
 
@@ -14,4 +17,40 @@ foreach ($group in $sourcegroups) {
   } else {
     $TargetWeb.SiteGroups.add($group.Name, $owner, $TargetWeb.site.owner, $group.description)
   }
+
+
+#Read more: https://www.sharepointdiary.com/2015/01/create-sharepoint-group-using-powershell.html#ixzz6INbhDxCV
+ #$TargetWeb.SiteGroups.Add($group.Name, $owner, $null, $group.description)
+ #$destinationGroup = $TargetWeb.SiteGroups["$group"]
+ #$destinationGroup.owner = $owner
+ #$destinationGroup.Update()
+}
+
+Write-Output "`n"
+
+$usergroupmembers = Import-Csv ./groupmembers.csv
+
+foreach ($user in $usergroupmembers) {
+    Write-Output $user.LoginName
+    $veruser = $TargetWeb.EnsureUser($user.LoginName)
+    if ($veruser -ne $null) {
+
+        $group = $TargetWeb.SiteGroups[$user.Group]
+        #Write-Output $group
+        $group.AddUser($veruser)
+    }
+   
+}
+
+$grouproles = Import-Csv ./grouproles.csv
+
+foreach ($role in $grouproles) {
+         Write-Output $role.Group
+        $siteGroup = $TargetWeb.SiteGroups[$role.Group]  
+        $roleAssignment = new-object Microsoft.SharePoint.SPRoleAssignment($sitegroup) 
+        $roleDefinition = $web.Site.RootWeb.RoleDefinitions[$role.Name]  
+        #Write-Output $roleDefinition
+        $roleAssignment.RoleDefinitionBindings.Add($roleDefinition)
+        $TargetWeb.RoleAssignments.Add($roleAssignment)  
+        $TargetWeb.Update()
 }
